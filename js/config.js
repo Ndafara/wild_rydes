29695ba1cbee4d874c2db65fc96c28cd4c83251d
Ndafara Tsamba <ntsamba@gmail.com>
2020-08-18 12:55:01 +0200

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_9Dn1SxxQ8', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5s93gpa6df4ga9moktcj2d8ggr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
